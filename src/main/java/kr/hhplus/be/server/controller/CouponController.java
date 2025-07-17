2d272b9147068a6ccc4e99af61ae16b55ef19fe7
rigollet <rigollet153@gmail.com>
2025-07-18 03:33:45 +0900

    시퀀스 다이어그램에 작성된 예외사항 추가

--- src/main/java/kr/hhplus/be/server/controller/CouponController.java
+++ src/main/java/kr/hhplus/be/server/controller/CouponController.java
@@ -1,6 +1,10 @@
 package kr.hhplus.be.server.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.dto.CouponResponse;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,12 @@ import java.util.List;
 public class CouponController {
 
     @Operation(summary = "쿠폰 발급", description = "선착순으로 사용자에게 쿠폰을 발급합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "쿠폰 발급 성공",
+                    content = @Content(schema = @Schema(implementation = CouponResponse.class))),
+            @ApiResponse(responseCode = "409", description = "쿠폰 모두 소진됨",
+                    content = @Content(schema = @Schema(example = "{\"message\": \"쿠폰이 모두 소진되었습니다.\"}")))
+    })
     @PostMapping("/issue/{userId}")
     public ResponseEntity<CouponResponse> issueCoupon(@PathVariable Long userId) {
         Long couponId = 0L;
--- src/main/java/kr/hhplus/be/server/controller/OrderController.java
+++ src/main/java/kr/hhplus/be/server/controller/OrderController.java
@@ -1,6 +1,10 @@
 package kr.hhplus.be.server.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.dto.OrderRequest;
 import kr.hhplus.be.server.dto.OrderResponse;
@@ -16,6 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @Operation(summary = "주문 및 결제", description = "상품 ID 및 수량 목록을 기반으로 주문 및 결제를 수행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "주문/결제 성공",
+                    content = @Content(schema = @Schema(implementation = OrderResponse.class))),
+            @ApiResponse(responseCode = "400", description = "잔액 부족",
+                    content = @Content(schema = @Schema(example = "{\"message\": \"잔액이 부족합니다.\"}"))),
+            @ApiResponse(responseCode = "409", description = "재고 부족",
+                    content = @Content(schema = @Schema(example = "{\"message\": \"재고가 부족한 상품이 있습니다.\"}")))
+    })
     @PostMapping
     public ResponseEntity<OrderResponse> order(@RequestBody OrderRequest request) {
         return ResponseEntity.ok(new OrderResponse("주문이 성공적으로 완료되었습니다.", 10000L));
