51601886109bee1333914c05c9e3c58e2fa166cf
rigollet <rigollet153@gmail.com>
2025-07-24 04:14:05 +0900

    잔액 충전 기능 구현

--- src/main/java/kr/hhplus/be/server/domain/User.java
+++ src/main/java/kr/hhplus/be/server/domain/User.java
@@ -1,4 +1,26 @@
 package kr.hhplus.be.server.domain;
 
 public class User {
+    private final Long userId;
+    private long amount;
+
+    public User(Long userId, long amount) {
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+    public void charge(long value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("충전 금액은 0보다 커야 합니다.");
+        }
+        this.amount += value;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
 }
