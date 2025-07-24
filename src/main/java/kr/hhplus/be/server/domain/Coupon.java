6133b10aeeac0742ef9e6dd56c719e11178f7ce2
rigollet <rigollet153@gmail.com>
2025-07-24 22:16:19 +0900

    쿠폰 발급 정책 도메인 코드 작성

--- src/main/java/kr/hhplus/be/server/domain/Coupon.java
+++ src/main/java/kr/hhplus/be/server/domain/Coupon.java
@@ -1,4 +1,40 @@
 package kr.hhplus.be.server.domain;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Coupon {
+    private final String id;
+    private final long userId;
+    private final int discountAmount;
+    private final LocalDateTime issuedAt;
+    private boolean used;
+
+    public Coupon(long userId, int discountAmount) {
+        this.id = UUID.randomUUID().toString(); // 고유 쿠폰 ID 생성
+        this.userId = userId;
+        this.discountAmount = discountAmount;
+        this.issuedAt = LocalDateTime.now();
+        this.used = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public LocalDateTime getIssuedAt() {
+        return issuedAt;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
 }
