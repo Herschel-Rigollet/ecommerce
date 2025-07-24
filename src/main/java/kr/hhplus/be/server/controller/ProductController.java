7f4511e987a58faa5aab67a839d9d0ed1f1c40fc
rigollet <rigollet153@gmail.com>
2025-07-24 17:05:52 +0900

    상품 조회 패키지 구조 작성

--- src/main/java/kr/hhplus/be/server/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/controller/ProductController.java
@@ -1,34 +1,4 @@
 package kr.hhplus.be.server.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import kr.hhplus.be.server.dto.ProductResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/products")
-@Tag(name = "Product API", description = "상품 조회 API")
 public class ProductController {
-
-    @Operation(summary = "상품 전체 조회", description = "모든 상품의 ID, 이름, 가격, 재고를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "조회 성공",
-                    content = @Content(schema = @Schema(implementation = ProductResponse.class)))
-    })
-    @GetMapping
-    public ResponseEntity<List<ProductResponse>> getProducts() {
-        return ResponseEntity.ok(List.of(
-                new ProductResponse(1L, "상품1", 1000L, 10),
-                new ProductResponse(2L, "상품2", 2000L, 5)
-        ));
-    }
 }
--- src/main/java/kr/hhplus/be/server/dto/ProductResponse.java
+++ src/main/java/kr/hhplus/be/server/dto/ProductResponse.java
@@ -1,13 +1,4 @@
 package kr.hhplus.be.server.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class ProductResponse {
-    private Long productId;
-    private String productName;
-    private Long price;
-    private int stock;
 }
