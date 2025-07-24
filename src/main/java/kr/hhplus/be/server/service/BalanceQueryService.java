950f51431a4b58e0d8e8ad33552e7ced4f2f2f8f
rigollet <rigollet153@gmail.com>
2025-07-24 16:36:57 +0900

    잔액 조회 기능 최소한의 코드로 통과하도록 BalanceQueryService 수정

--- src/main/java/kr/hhplus/be/server/service/BalanceQueryService.java
+++ src/main/java/kr/hhplus/be/server/service/BalanceQueryService.java
@@ -1,4 +1,18 @@
 package kr.hhplus.be.server.service;
 
+import kr.hhplus.be.server.domain.BalanceRepository;
+import kr.hhplus.be.server.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BalanceQueryService {
+    private final BalanceRepository repository;
+
+    public BalanceQueryService(BalanceRepository repository) {
+        this.repository = repository;
+    }
+
+    public User getBalance(Long userId) {
+        return repository.findByUserId(userId);
+    }
 }
