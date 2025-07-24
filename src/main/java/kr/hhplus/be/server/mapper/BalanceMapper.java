95fba113803e42be4ada6def2edf6875a92b9c54
rigollet <rigollet153@gmail.com>
2025-07-24 16:49:08 +0900

    잔액 Mapper 클래스 작성

--- src/main/java/kr/hhplus/be/server/mapper/BalanceMapper.java
+++ src/main/java/kr/hhplus/be/server/mapper/BalanceMapper.java
@@ -1,4 +1,10 @@
 package kr.hhplus.be.server.mapper;
 
+import kr.hhplus.be.server.domain.User;
+import kr.hhplus.be.server.dto.BalanceResponse;
+
 public class BalanceMapper {
+    public static BalanceResponse toDto(User balance) {
+        return new BalanceResponse(balance.getUserId(), balance.getAmount());
+    }
 }
