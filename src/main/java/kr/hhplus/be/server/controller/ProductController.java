ffd31f0694e82a49441cfb9642d519eec444bdc7
rigollet <rigollet153@gmail.com>
2025-07-24 17:15:41 +0900

    상품 조회 dto, mapper, controller 작성

--- src/main/java/kr/hhplus/be/server/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/controller/ProductController.java
@@ -1,4 +1,27 @@
 package kr.hhplus.be.server.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import kr.hhplus.be.server.dto.ProductResponse;
+import kr.hhplus.be.server.mapper.ProductMapper;
+import kr.hhplus.be.server.service.ProductQueryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductQueryService service;
+
+    public ProductController(ProductQueryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    @Operation(summary = "전체 상품 목록 조회", description = "현재 구매 가능한 전체 상품 목록을 조회합니다.")
+    public List<ProductResponse> getProducts() {
+        return ProductMapper.toDtoList(service.getAllProducts());
+    }
 }
--- src/main/java/kr/hhplus/be/server/mapper/ProductMapper.java
+++ src/main/java/kr/hhplus/be/server/mapper/ProductMapper.java
@@ -1,4 +1,16 @@
 package kr.hhplus.be.server.mapper;
 
+import kr.hhplus.be.server.domain.Product;
+import kr.hhplus.be.server.dto.ProductResponse;
+
+import java.util.List;
+
 public class ProductMapper {
+    public static ProductResponse toDto(Product p) {
+        return new ProductResponse(p.getId(), p.getName(), p.getPrice(), p.getStock());
+    }
+
+    public static List<ProductResponse> toDtoList(List<Product> products) {
+        return products.stream().map(ProductMapper::toDto).toList();
+    }
 }
