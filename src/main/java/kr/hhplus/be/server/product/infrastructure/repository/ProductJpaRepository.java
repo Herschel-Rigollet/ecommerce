f11fa35a015b8f976db9120b74fdb68347a1eb2f
rigollet <rigollet153@gmail.com>
2025-08-08 05:38:11 +0900

    재고 관리에 비관적 락 적용

--- src/main/java/kr/hhplus/be/server/product/infrastructure/repository/ProductJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/product/infrastructure/repository/ProductJpaRepository.java
@@ -1,7 +1,18 @@
 package kr.hhplus.be.server.product.infrastructure.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
+// 재고 관리: 비관적 락
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.productId = :id")
+    Optional<Product> findByIdForUpdate(@Param("id") Long id);
 }
