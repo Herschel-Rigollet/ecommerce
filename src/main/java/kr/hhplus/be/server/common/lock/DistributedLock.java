06024c516b0ec4ccc27f2b58ea952ecd11a30f87
rigollet <rigollet153@gmail.com>
2025-08-14 23:36:56 +0900

    분산락 어노테이션 정의

---
+++ src/main/java/kr/hhplus/be/server/common/lock/DistributedLock.java
@@ -0,0 +1,36 @@
+package kr.hhplus.be.server.common.lock;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributedLock {
+    /**
+     * 락 키 (SpEL 표현식 지원)
+     */
+    String key();
+
+    /**
+     * 락 대기시간 (초)
+     */
+    long waitTime() default 5L;
+
+    /**
+     * 락 점유시간 (초)
+     */
+    long leaseTime() default 10L;
+
+    /**
+     * 시간 단위
+     */
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    /**
+     * 락 획득 실패 시 예외 메시지
+     */
+    String failMessage() default "락 획득에 실패했습니다.";
+}
